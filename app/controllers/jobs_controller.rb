f12e8a1daebc913498f87110aaf742fdea7d7259
heric <hericpeng@gmail.com>
2017-04-11 08:03:43 +0800

    step 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,49 +1,55 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,  :create,:edit, :update, :destroy]
-  def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
-  end
-
-  def show
-    @job = Job.find(params[:id])
-  end
-
-  def new
-    @job = Job.new
-  end
-
-  def create
-    @job = Job.new(job_params)
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+    def index
+        @jobs = case params[:order]
+                when 'by_lower_bound'
+                    Job.published.order('wage_lower_bound DESC')
+                when 'by_upper_bound'
+                    Job.published.order('wage_upper_bound DESC')
+                else
+                    Job.published.order('created_at DESC')
+        end
     end
-  end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path
-    else
-      render:edit
+    def show
+        @job = Job.find(params[:id])
     end
-  end
 
-  def destroy
-    @job = Job.find(params[:id])
-    @job.destroy
-      redirect_to jobs_path
+    def new
+        @job = Job.new
+    end
 
-  end
+    def create
+        @job = Job.new(job_params)
+        if @job.save
+            redirect_to jobs_path
+        else
+            render :new
+        end
+    end
 
-  private
+    def edit
+        @job = Job.find(params[:id])
+    end
 
-  def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
-  end
+    def update
+        @job = Job.find(params[:id])
+        if @job.update(job_params)
+            redirect_to jobs_path
+        else
+            render:edit
+        end
+    end
+
+    def destroy
+        @job = Job.find(params[:id])
+        @job.destroy
+        redirect_to jobs_path
+    end
+
+    private
+
+    def job_params
+        params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
+    end
 end
